a68b9225f5d8c14404be92a0f4f8e6ff9c0d4fac
lee-huichan <hchan0511@gmail.com>
2025-03-19 14:32:43 +0900

    new file:   "\353\254\270\354\236\220\354\227\264 \354\227\260\354\202\260\354\236\220.py" 	new file:   "\354\225\240\352\265\255\352\260\200.py"

---
+++ 문자열 연산자.py
@@ -0,0 +1,56 @@
+print("10+20")
+print("안녕하세요"+"..!")
+print("10+20"+"문자열"+"문자열")
+
+print("안녕하세요"*3)
+
+print("# 문자 선택 연산자")
+print("안녕하세요"[0])
+print("안녕하세요"[1])
+print("안녕하세요"[2])
+print("안녕하세요"[3])
+print("안녕하세요"[4])
+
+print('"상명대학교"[0:3]', '상명대학교'[0:2])
+print('"상명대학교"[0:1]', "상명대학교"[1:3])
+
+print(len("상명대학교를 졸업하고 나서"))
+
+print(type("안녕하세요"))
+print(type(len("상명대학교에 입학한다면")))
+
+print(type(124))
+print(type(25.336))
+
+print(6*7)
+print(5/7)
+
+print("8/3:", 8/3)
+print("8//3:", 8//3)
+print(8%3)
+print(8**3)
+
+pi = 202315167
+print(pi)
+
+abc = 20
+abc += 20
+abc -= 10
+abc *= 5
+abc /= 2
+print("abc:", abc)
+
+string = "이디야커피"
+string += " 갈라면"
+string += " 어디로 가야 하나요"
+print("string:", string)
+
+abcd=input("커피는=")
+print(abcd)
+
+# 입력을 받습니다
+string=input("입력>")
+
+# 출력을 합니다
+print("입력:", string)
+print("자료형", type(string))
